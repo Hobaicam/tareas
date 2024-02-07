f9b316ae5582e785d08f6c480fa5c410a45d8d96
Hobaicam <156662790+Hobaicam@users.noreply.github.com>
2024-02-07 07:43:38 -0400

    Add files via upload

---
+++ ejercicio 5.py
@@ -0,0 +1,15 @@
+
+nums = [1,2,3,4]
+def split_into_pairs(lst):
+        x=0
+        return[lst[x], lst[x+1]] for x in a range(0,len(lst),2)
+
+pairs=split_into_pairs(nums)
+print(pairs)
+
+#primer_par=pairs[0] #escribir el num 2 la cantidad de veces del 1
+
+#segundo_par=pairs[1] #escribir el num 2 la cantidad de veces del 1 
+
+#total= primer_par+segundo_par
+
